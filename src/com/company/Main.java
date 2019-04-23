7f96dfc8235e3a7fe9e4b26586e794395692a096
Mihnea <mihneabulearca@gmail.com>
2019-04-23 18:18:30 +0300

    Exercitiul 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,27 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// "# ## ### #### ### ## #"
-		int n=7,i;
-		String s1="",s2="",s3="",s4="",s5="";
-		for(i=1; i<=n; i++) {
-			if (i==1) {
-				s1="# ";
-			}
-			else if (i==2||i==6) {
-				s2="## ";
-			}
-			else if (i==3||i==5) {
-				s3="### ";
-			}
-			else if (i==4) {
-				s4="#### ";
-			}
-			else if (i==7) {
-				s5="#";
-			}
-		}
-		System.out.println(s1+s2+s3+s4+s3+s2+s5);
+		int n=6, nrPasi=1;
+		while (n!=1) {
+		    if (n%2==0) {
+		        n=n/2;
+            }
+		    else {
+		        n=3*n+1;
+            }
+		    nrPasi+=1;
+        }
+		System.out.println(nrPasi);
     }
 
     }

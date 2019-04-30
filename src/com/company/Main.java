aa873bc500cb713c6beba46e56f3e80f2cf6c3d0
Mihnea <mihneabulearca@gmail.com>
2019-04-30 17:51:49 +0300

    Exercitiul 10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-		int n=5;
-		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=i; j++) {
-				System.out.println(j);
+		int [] n={1,2,3,4,5,6,7,8,9,10,11,12};
+		int j=5;
+		int s=0;
+		for(int i=0; i<=n.length-1; i++) {
+			if (j!=0) {
+				if(n[i]%2==0) {
+					s+=n[i];
+					j-=1;
+				}
 			}
 		}
+		System.out.println(s);
     }
 
     }

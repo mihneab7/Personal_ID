7ef26e58f265af982505166f374a2744ee572f6e
Mihnea <mihneabulearca@gmail.com>
2019-04-30 19:51:01 +0300

    Exercitiul 14

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    	int n=4;
-    	for(int i=1; i<=n; i++) {
-    		System.out.println("go");
+    	int n=3;
+    	boolean a=true;
+    	for (;;) {
+    		if (a) {
+    			for (int i=1; i<=n; i++) {
+    				System.out.println("going");
+				}
+    			a=false;
+			}
 		}
-    	System.out.println("gata");
 		}
     }

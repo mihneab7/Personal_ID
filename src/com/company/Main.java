ad99d0084563500512beb4a504832c8a69cda56f
Mihnea <mihneabulearca@gmail.com>
2019-04-30 18:11:46 +0300

    Exercitiul 11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,63 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-		int [] n={1,2,3,4,5,6,7,8,9,10,11,12};
-		int j=5;
-		int s=0;
-		for(int i=0; i<=n.length-1; i++) {
-			if (j!=0) {
-				if(n[i]%2==0) {
-					s+=n[i];
-					j-=1;
-				}
-			}
+//		int n=8;
+//		if (n<0||n>9) {
+//			System.out.println("numar gresit");
+//		}
+//		else if (n==0) {
+//			System.out.println("zero");
+//		}
+//		else if (n==1) {
+//			System.out.println("unu");
+//		}
+//		else if (n==2) {
+//			System.out.println("doi");
+//		}
+//		else if (n==3) {
+//			System.out.println("trei");
+//		}
+//		else if (n==4) {
+//			System.out.println("patru");
+//		}
+//		else if (n==5) {
+//			System.out.println("cinci");
+//		}
+//		else if (n==6) {
+//			System.out.println("sase");
+//		}
+//		else if (n==7) {
+//			System.out.println("sapte");
+//		}
+//		else if (n==8) {
+//			System.out.println("opt");
+//		}
+//		else if (n==9) {
+//			System.out.println("noua");
+//		}
+		int n=8;
+		switch (n) {
+			case 0: System.out.println("zero");
+			break;
+			case 1: System.out.println("unu");
+			break;
+			case 2: System.out.println("doi");
+			break;
+			case 3: System.out.println("trei");
+			break;
+			case 4: System.out.println("patru");
+			break;
+			case 5: System.out.println("cinci");
+			break;
+			case 6: System.out.println("sase");
+			break;
+			case 7: System.out.println("sapte");
+			break;
+			case 8: System.out.println("opt");
+			break;
+			case 9: System.out.println("noua");
+			break;
+			default: System.out.println("numar gresit");
 		}
-		System.out.println(s);
-    }
-
+		}
     }

a68593dbc826a13877e0bed172d0305573003d82
Mihnea <mihneabulearca@gmail.com>
2019-04-30 15:59:25 +0300

    Exercitiul 6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-		int n=3;
-		while (n!=0) {
-			System.out.println(n);
-			n-=1;
+		int n=7;
+		int i=1;
+		if (n<=0) {
+			System.out.println("nu sunt numere naturale impare");
+			System.exit(0);
+		} else {
+			while (i <= 2 * n) {
+				if (i % 2 == 1) {
+					System.out.println(i);
+				}
+				i += 1;
+			}
 		}
     }
 

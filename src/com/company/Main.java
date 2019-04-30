4c8c0e4a464de14656986e3f9a5c5e4063fbe7a3
Mihnea <mihneabulearca@gmail.com>
2019-04-30 15:37:50 +0300

    Exercitiul 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-		int [] n={1,4,3,5};
-		int nrPare=0;
-		int nrImpare=0;
-		for(int i=0; i<=n.length-1; i++) {
-		    if (n[i]%2==0) {
-		        nrPare+=1;
-            } else if (n[i]%2==1) {
-		        nrImpare+=1;
-            }
-        }
-		System.out.println(nrPare);
-		System.out.println(nrImpare);
+		int n=3;
+		while (n!=0) {
+			System.out.println(n);
+			n-=1;
+		}
     }
 
     }

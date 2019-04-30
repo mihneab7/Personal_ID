bda32f1cd748c00a35ab4bdbb22aa8e1f5d46381
Mihnea <mihneabulearca@gmail.com>
2019-04-30 15:24:05 +0300

    Exercitiul 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,20 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-		int [] n={2,4,1,6,8,15};
-		int max=-1;
-		for (int i=0; i<=n.length-1; i++) {
-			if (n[i]%2==1) {
-				if (n[i]>=max) {
-					max=n[i];
-				}
-			}
-		}
-		if (max==-1) {
-			System.out.println("no");
-		} else {
-			System.out.println(max);
-		}
+		int [] n={1,4,3,5};
+		int nrPare=0;
+		int nrImpare=0;
+		for(int i=0; i<=n.length-1; i++) {
+		    if (n[i]%2==0) {
+		        nrPare+=1;
+            } else if (n[i]%2==1) {
+		        nrImpare+=1;
+            }
+        }
+		System.out.println(nrPare);
+		System.out.println(nrImpare);
     }
 
     }

185d20f158059e423a486dbb2eec4a3462a298ba
Mihnea <mihneabulearca@gmail.com>
2019-04-30 17:41:28 +0300

    Exercitiul 9

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-		int a=2,b=3;
-		int rez=1;
-		for(int i=1; i<=b; i++) {
-			rez*=a;
+		int n=5;
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.println(j);
+			}
 		}
-		System.out.println(rez);
     }
 
     }

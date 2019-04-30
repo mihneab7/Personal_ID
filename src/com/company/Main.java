cea3daac23629894e6ccccde4b414d765a0acd88
Mihnea <mihneabulearca@gmail.com>
2019-04-30 20:45:23 +0300

    Exercitiul 17

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    	int n=134526, c, t=0, s=0;
+    	int n=1234, c, m, t=0, sp=0, si=0;
     	while(n!=0) {
     		c=n%10;
     		t+=1;
     		if(t%2==0) {
-    			s+=c;
+    			sp+=c;
+			}
+    		else {
+    			si+=c;
 			}
     		n/=10;
 		}
-    	System.out.println(s);
+    	m=sp-si;
+    	System.out.println(m);
 		}
     }

612048ac4629b855c9d8b50c825b69949a5b9462
Mihnea <mihneabulearca@gmail.com>
2019-04-30 15:14:15 +0300

    Exercitiul 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-		int n=6, nrPasi=1;
-		while (n!=1) {
-		    if (n%2==0) {
-		        n=n/2;
-            }
-		    else {
-		        n=3*n+1;
-            }
-		    nrPasi+=1;
-        }
-		System.out.println(nrPasi);
+		int [] n={2,4,1,6,8,15};
+		int max=-1;
+		for (int i=0; i<=n.length-1; i++) {
+			if (n[i]%2==1) {
+				if (n[i]>=max) {
+					max=n[i];
+				}
+			}
+		}
+		if (max==-1) {
+			System.out.println("no");
+		} else {
+			System.out.println(max);
+		}
     }
 
     }
